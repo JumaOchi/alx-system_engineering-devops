69366b5f14be606882b95f2e0206af357f1c9f89
JumaOchi <ochiengjuma95@gmail.com>
2022-10-17 20:54:18 +0300

    update to puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#installing flask from pip3
+# Script that installs a package, here: flask
 
 package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
